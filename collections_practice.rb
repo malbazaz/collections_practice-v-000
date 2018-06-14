2527350ea54bd5bb02bfe3997f2cc987d2929b84
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-14 03:53:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,3 +18,21 @@ def sort_array_desc(array)
   end 
   newarray 
 end 
+
+def sort_array_char_count(array)
+  newarray = []
+  array.each do |string|
+  number = string.length
+  newarray << number
+  newarray.sort do |a,b|
+    if a==b 
+      0 
+    elsif a>b 
+    -1 
+    elsif 
+    1 
+  end
+end 
+end 
+newarray
+end 
