e7f0caba6bf86a770cce18b8233fcfbc84fd3e87
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-14 04:00:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,7 +21,13 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a,b|
-    a.length<=>b.length 
+    if a.length == b.length  
+      0 
+      elsif a.length < b.length  
+      -1 
+      elsif a.length > b.length  
+      1 
+    end 
   end 
     array
   end 
